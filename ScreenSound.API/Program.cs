23ea7dd5421c5874cafdeac9cc483d49aa65abd0
Eduardombc <108088699+Eduardombc@users.noreply.github.com>
2025-07-30 16:10:50 -0300

    Ajuste Swagger

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,18 +1,26 @@
 using System.Text.Json.Serialization;
-using Microsoft.AspNetCore.Mvc;
 using ScreenSound.API.EndPoints;
 using ScreenSound.Banco;
 using ScreenSound.Modelos;
+using Microsoft.OpenApi.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ScreenSoundContext>();
 builder.Services.AddTransient<DAL<Artista>>();
 builder.Services.AddTransient<DAL<Musica>>();
+
+builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 var app = builder.Build();
 
 app.AddEndPointsArtistas();
 app.AddExtensionsMusicas();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
