c67246060a5eb1568547df4f240da1eff590a1e9
Eduardombc <eduardomunizcontato@gmail.com>
2025-08-16 20:57:58 -0300

    Inicio Autenticação

--- ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -43,35 +43,6 @@ public class ScreenSoundContext : DbContext
         modelBuilder.Entity<Musica>()
             .HasMany(c => c.Generos)
             .WithMany(c => c.Musicas);
-
-        modelBuilder.Entity<Artista>().HasData(
-            new Artista { Id = 1, Nome = "Djavan", Bio = "Um dos maiores cantores e compositores da MPB...", FotoPerfil = "url-da-foto-djavan" },
-            new Artista { Id = 2, Nome = "U2", Bio = "Banda de rock irlandesa formada em 1976...", FotoPerfil = "url-da-foto-u2" },
-            new Artista { Id = 3, Nome = "Queen", Bio = "Banda de rock britânica formada em Londres em 1970...", FotoPerfil = "url-da-foto-queen" }
-        );
-
-        modelBuilder.Entity<Genero>().HasData(
-            new Genero { Id = 1, Nome = "Rock", Descricao = "Gênero de música popular que se desenvolveu durante e após a década de 1950." },
-            new Genero { Id = 2, Nome = "MPB", Descricao = "Música popular brasileira, um estilo musical que surgiu no Brasil." },
-            new Genero { Id = 3, Nome = "Pop", Descricao = "Música popular, geralmente orientada para o mercado comercial." }
-        );
-
-        modelBuilder.Entity<Musica>().HasData(
-            new Musica { Id = 1, Nome = "Sina", AnoLancamento = 1982, ArtistaId = 1 },
-            new Musica { Id = 2, Nome = "Oceano", AnoLancamento = 1989, ArtistaId = 1 },
-            new Musica { Id = 3, Nome = "With or Without You", AnoLancamento = 1987, ArtistaId = 2 },
-            new Musica { Id = 4, Nome = "One", AnoLancamento = 1991, ArtistaId = 2 },
-            new Musica { Id = 5, Nome = "Bohemian Rhapsody", AnoLancamento = 1975, ArtistaId = 3 }
-        );
-
-        modelBuilder.Entity("GeneroMusica").HasData(
-            new { MusicasId = 1, GenerosId = 2 },
-            new { MusicasId = 2, GenerosId = 2 },
-            new { MusicasId = 3, GenerosId = 1 },
-            new { MusicasId = 4, GenerosId = 1 },
-            new { MusicasId = 5, GenerosId = 1 },
-            new { MusicasId = 5, GenerosId = 3 }
-        );
     }
 
 
